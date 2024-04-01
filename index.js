606e73076f375e7bfad4ec06ed6c1b2580724d66
surajkhan123 <100072865+surajkhan123@users.noreply.github.com>
2024-04-01 19:49:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer" , "Blogger" , "Web Developer", "Business Analyst"],   
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay:1000,
+    loop:true
+});
